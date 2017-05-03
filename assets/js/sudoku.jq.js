fe700021374943e3c0b53d1669072192b303d182
Chris45 <christophe.hebert45@gmail.com>
2017-05-04 00:42:07 +0200

    Corrections orthographiques

--- assets/js/sudoku.jq.js
+++ assets/js/sudoku.jq.js
@@ -1,18 +1,18 @@
 /*******************************************************************************
  * Classe Js pour résolution de Sudoku
- * Cahllenge proposé par Yoomap
+ * Challenge proposé par Yoomap
  *
  * @author : LECOMTE Cyril <cyrhades76@gmail.com>
  * @dependencies : jQuery, sudoku
  * @since: 20/10/2016
- * @history : 
+ * @history :
  *            - 20/10/2016 Création interface
  *            - 22/10/2016 Résolution du Sudoku
  *            - 28/10/2016 Générateur de grille de Sudoku / amélioration IHM
  *******************************************************************************/
 
  /**
-  * Vide la grille (remise à Zero)
+  * Vide la grille (remise à Zéro)
   */
  var allEmptyCase = function()
  {
@@ -23,13 +23,13 @@
  };
 
 /**
- * Ecriture du resultat du sudoku dans la grille
+ * Ecriture du résultat du sudoku dans la grille
  */
 var explodeSudokuString = function(aResult) {
 	var i = 0;
 	// On boucle sur les lignes
 	for (var line = 1; line<=9; line++)	{
-		// On boucle sur les colonne
+		// On boucle sur les colonnes
 		for (var row = 1; row<=9; row++) {
 			if (parseInt(aResult[i]) > 0 && parseInt(aResult[i]) < 10) {
 				if ($('#input_'+line+'_'+row).val() != parseInt(aResult[i])) {
@@ -47,13 +47,13 @@ var explodeSudokuString = function(aResult) {
 };
 
 /**
- * Converti la grille en chaine
+ * Convertie la grille en chaine
  */
 var implodeSudokuString = function()	{
 	var sResult = '';
 	// On boucle sur les lignes
 	for (var line = 1; line<=9; line++)	{
-		// On boucle sur les colonne
+		// On boucle sur les colonnes
 		for (var row = 1; row<=9; row++) {
 			var i = $('#input_'+line+'_'+row).val();
 			if (parseInt(i) > 0) {
@@ -74,12 +74,12 @@ var implodeRandomSudokuString = function()	{
 	var gridBase = [];
 	// On va mettre 4 chiffres de 1 à 9 avec comme clef 1 à 81 en random également
 	for (var nbNumber = 1; nbNumber<=4; nbNumber++)	{
-		gridBase[(Math.floor((81-1)*Math.random())+1)] = (Math.floor((9-1)*Math.random())+1); 
+		gridBase[(Math.floor((81-1)*Math.random())+1)] = (Math.floor((9-1)*Math.random())+1);
 	}
 
 	// On boucle sur les lignes
 	for (var line = 1; line<=9; line++)	{
-		// On boucle sur les colonne
+		// On boucle sur les colonnes
 		for (var row = 1; row<=9; row++) {
 			var i = gridBase[(line*row)];
 			if (parseInt(i) > 0) {
@@ -93,7 +93,7 @@ var implodeRandomSudokuString = function()	{
 };
 
 /**
- * Genere une grille factice
+ * Genère une grille factice
  */
 var cleanGridForNewSudoku = function(aSudoku, iDifficulty) {
 	var iNbDelete = 0;
@@ -120,11 +120,11 @@ var generateSudoku = function(iDifficulty) {
 		var solver = sudoku_solver()
 		var aResult = solver(sBase,1); // On ne prend qu'un seul resultat
 		if (typeof aResult[0] != 'undefined') {
-			
+
 			if (iDifficulty == 0 || typeof iDifficulty == 'undefined' || isNaN(iDifficulty)) {
 				iDifficulty = 35;
 			}
-			// On complete la grille
+			// On complète la grille
 			explodeSudokuString(cleanGridForNewSudoku(aResult[0],iDifficulty));
 			$('.case_solution').addClass('case_strict').removeClass('case_solution');
 		} else {
@@ -141,12 +141,12 @@ var resolveSudoku = function() {
 	$('.solution').removeClass('solution');
 	// On crée une chaine à partir de chaque ligne
 	var sBase = implodeSudokuString();
-	
+
 	if (sBase.length >= 81) {
 		var solver = sudoku_solver();
-		var aResult = solver(sBase,1); // On ne prend qu'un seul resultat
+		var aResult = solver(sBase,1); // On ne prend qu'un seul résultat
 		if (typeof aResult[0] != 'undefined') {
-			// On complete la grille
+			// On complète la grille
 			explodeSudokuString(aResult[0]);
 		} else {
 			alert('Votre Sudoku ne peut pas être résolu.');
@@ -169,8 +169,8 @@ $.fn.createSudokuGrid = function(options) {
 		btnEmptyGrid : 		"#empty"
 
 	};
-	
-	// Les parametres du sudoku
+
+	// Les paramètres du sudoku
 	var params = $.extend(defaultOptions,  options );
 
 	$(params.btnResolve).on('click', function() {
@@ -184,15 +184,15 @@ $.fn.createSudokuGrid = function(options) {
 	$(params.btnEmptyGrid).on('click', function() {
 		allEmptyCase();
 	});
-	
+
     this.each(function() {
 	    var sEmptySudoku = '';
-	    
+
 		// 3 Lignes contenant ...
 		for (var line = 1; line<=3; line++)
 		{
 			sEmptySudoku += '<div class="grid">';
-			// ... 3 Bloque contannt eux même ...
+			// ... 3 Blocks contenant eux mêmes ...
 			for (var block = 1; block<=3; block++)
 			{
 				sEmptySudoku += '<div class="block">';
@@ -204,10 +204,10 @@ $.fn.createSudokuGrid = function(options) {
 					sEmptySudoku += '<div class="case">';
 					sEmptySudoku += '<input id="input_'+numLine+'_'+numCol+'" data-row="'+numLine+'" data-col="'+numCol+'" data-block="'+line+''+block+'" class="number" type="text" min="1" max="9" maxlength="1" />';
 					sEmptySudoku += '</div>';
-				}	
+				}
 				sEmptySudoku += '</div>'; // Fin de block
 			}
-			sEmptySudoku += '</div>'; // Fin de line
+			sEmptySudoku += '</div>'; // Fin de ligne
 		}
 		// On affiche notre grille sudoku
 		$(this).css({width: params.size, height: params.size}).html(sEmptySudoku);
@@ -223,7 +223,7 @@ $.fn.createSudokuGrid = function(options) {
 	$('div.case', $(this)).css({float: 'left', width: '33%', height: '33%'});
 	$('input.number', $(this)).css({width: '100%', height: '100%', 'vertical-align': 'middle', 'text-align':'center'});
 
-    // Action de controle sur les champs
+    // Action de contrôle sur les champs
     $('input.number').controlFieldInput()
     	// Prise du focus au survol
 	    .on('mouseover',function(){
@@ -236,11 +236,11 @@ $.fn.createSudokuGrid = function(options) {
 };
 
 /**
- * Control la validation d'un champ
+ * Contrôle la validation d'un champ
  */
  $.fn.controlFieldInput = function() {
  	/**
- 	 * La methode de controle de la validite d'un bloc
+ 	 * La méthode de contrôle de la validité d'un bloc
  	 */
  	var zoneIsCorrect = function(sType, oCurrentObj) {
  		var aNumber = [];
@@ -248,21 +248,21 @@ $.fn.createSudokuGrid = function(options) {
 		var objCible = $('[data-'+sType+'="'+( $(oCurrentObj).attr('data-'+sType) )+'"]');
 		var iCurrentValue = oCurrentObj.val();
 
-		// De base on considére qu'il n'y a pas d'erreur
+		// De base on considère qu'il n'y a pas d'erreur
 		objCible.removeClass('error_'+sType);
 
 		// On boucle sur toutes les cibles
 		$(objCible).each(function(){
-			// Les case peuvent être vide
+			// Les cases peuvent être vides
 			if ($(this).val() != '') {
-				// On vérifie pas la case elle même
-				if ($(this).val() == iCurrentValue 
-					&& 
+				// On ne vérifie pas la case elle même
+				if ($(this).val() == iCurrentValue
+					&&
 					oCurrentObj.attr("id") != $(this).attr("id")
 				) {
-					// On marque l'eerur sur tous le block
+					// On marque l'erreur sur tout le block
 					objCible.addClass('error_'+sType);
-					return; // On stop y a déja une erreur
+					return; // On stoppe s'il y a déjà une erreur
 				} else if(aNumber.indexOf($(this).val()) >= 0) {
 					// Il y a une erreur quand même (mais ne provient pas de la donnée courante)
 					objCible.addClass('error_'+sType);
@@ -275,8 +275,8 @@ $.fn.createSudokuGrid = function(options) {
  	};
 
 	return this.each(function() {
-		// Au focus on sélectionne pour pouvoir remplacer 
-		// une valeur sans difficulte
+		// Au focus on sélectionne pour pouvoir remplacer
+		// une valeur sans difficulté
 		$(this).on("focus", function(event) {
 			$(this).select();
 		});
@@ -294,18 +294,18 @@ $.fn.createSudokuGrid = function(options) {
 				return false;
 			}
 
-			// Ca doit être un chiffre
+			// ça doit être un chiffre
 			return /\d/.test(String.fromCharCode(code));
 		});
 
-		// sa valeur  ne doit pas se retrouver sur la même ligne, 
-		// ni sur la même colonne, ni dans son bloc.
+		// sa valeur ne doit pas se retrouver sur la même ligne,
+		// ni sur la même colonne, ni dans son bloc
 		$(this).on("keyup", function(event) {
 			// On boucle sur le bloc complet
 			zoneIsCorrect('block', $(this));
-			// On boucle sur la ligne complete
+			// On boucle sur la ligne complète
 			zoneIsCorrect('row', $(this));
-			// On boucle sur la colonne complete
+			// On boucle sur la colonne complète
 			zoneIsCorrect('col', $(this));
 			// On reprend le focus pour le confort de l'utilisateur
 			$(this).focus();
